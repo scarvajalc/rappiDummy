cf65eb5bdc2165b417ea0f4d2cbad07bc59d8310
scarvajalc <santicarccc@gmail.com>
2020-12-09 12:05:27 -0500

    crear y resumen endpoint implementados

--- db.py
+++ db.py
@@ -1,17 +1,18 @@
 from pydantic import BaseModel
+from datetime import date
 
 
 class Orden(BaseModel):
     id: int
-    fecha: str
+    fecha: date
     usuario: str
     tienda: str
     valor: float
 
 
 ordenes = {
-    1: Orden(id=1, fecha="20-11-2020", usuario="pepe", tienda="dominos", valor=2000),
-    2: Orden(id=2, fecha="20-11-2020", usuario="juan", tienda="pizzhut", valor=3000)
+    1: Orden(id=1, fecha="2020-11-20", usuario="pepe", tienda="dominos", valor=2000),
+    2: Orden(id=2, fecha="2020-11-20", usuario="juan", tienda="pizzhut", valor=3000)
 }
 
 
@@ -21,3 +22,11 @@ def obtener_ordenes():
     for e in ordenes:
         lista_ordenes.append(ordenes[e])
     return lista_ordenes
+
+
+def crear_orden(orden: Orden):
+    if orden.id in ordenes:
+        return False
+    else:
+        ordenes[orden.id] = orden
+        return True
--- main.py
+++ main.py
@@ -8,3 +8,13 @@ app = FastAPI()
 async def obtener_ordenes():
     ordenes = db.obtener_ordenes()
     return ordenes
+
+
+@app.post("/ordenes/crear/")
+async def crear_orden(orden: db.Orden):
+    creada_exitosamente = db.crear_orden(orden)
+    if creada_exitosamente:
+        return {"mensaje": "Orden creada correctamenteo"}
+    else:
+        raise HTTPException(
+            status_code=400, detail="error, orden con ese id ya exisitia")
